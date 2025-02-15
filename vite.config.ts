8abfd4635324d8743eabb35f4293d8e145e3ff70
firas nouira <firasnouira33@gmail.com>
2025-02-15 22:00:11 +0100

    fixed base paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({ 
-    base: "/path_holes_detector", 
+    base: "/", 
     plugins: [react()],
     server:{port:3000},
     preview:{
